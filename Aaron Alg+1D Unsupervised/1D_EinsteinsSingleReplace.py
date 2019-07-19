00ec2ad77105fc0617a99987f9c576b0ffdc9712
aholman22 <ajholman@uchicago.edu>
2019-07-19 14:31:31 -0700

    Tried fixing 1 direction, but unable to get it to converge.

--- Aaron Alg+1D Unsupervised/1D_EinsteinsSingleReplace.py
+++ Aaron Alg+1D Unsupervised/1D_EinsteinsSingleReplace.py
@@ -36,7 +36,7 @@ class time_Efield(object):
 e_charge = 1.602176565e-19
 h_plank = 6.62607004e-34
 c_speed = 299792458
-x = 1000 #Number of points to test in frequeny and time domain
+x = 2001 #Number of points to test in frequeny and time domain
 
 #Inputs
 ph_en = 8300 #in the uint of eV
@@ -48,60 +48,46 @@ np.random.seed(1) #Set seed if want to create reproducable results
 Aks = np.ones(n) #np.random.rayleigh(size=n) is true variation
 wks = np.ones(n)*w_cen #np.random.normal(w_cen,10**-4*w_cen,n) true variation
 phiks = np.random.random(n)*2*np.pi #random values between 0 and 2pi
-tks = np.arange(1,n+1)*1e-15 #Will probably introduce variation later
+tks = np.arange(0,n)*1e-15 - 0.5e-15 #Will probably introduce variation later
 sigmaks = np.ones(n)*0.3e-15 #fixed gaussian width
 #Time domain
-t=np.linspace(-5e-15,(n+1)*1e-15 + 5e-15,x)
+t=np.linspace(-10e-15,10e-15,x)
 #Frequency Domain
 wrange = np.array([1-1e-3,1+1e-3])*w_cen #Domain centered around central frequency
 w=np.arange(wrange[0],wrange[1],(wrange[1]-wrange[0])/x)
 
 
-
-
-
-
 #Generate electric fields in time and frequency domains
 efieldtmpt = time_Efield(Aks,wks,phiks,tks,sigmaks,t)
 TimeE = efieldtmpt.time_field()
-FreqE = np.fft.fft(TimeE)
 
-FreqAmp = np.abs(FreqE)
+t_shift = np.fft.ifftshift(t)
+efieldtmpt_shift = time_Efield(Aks,wks,phiks,tks,sigmaks,t_shift)
+tfield_shift = efieldtmpt_shift.time_field()
+
+ffield_fft = np.fft.fft(tfield_shift)
+
+#Define amplitude of the electric field
+FreqAmp = np.abs(ffield_fft)
 
 def GSFreq(iterations,FreqAmp):
+    #set seed if you want to test same initial conditions
     np.random.seed(1)
+    #generate random inital phase
     phase_freq = 2*np.pi*np.random.random(len(FreqAmp))
     for i in range(iterations):
+        #Multiply phase with initial amplitude in frequency space
         Ew = np.exp(1j*phase_freq)*FreqAmp
+        #Perform inverse fourier transform
         Guess_Time = np.fft.ifft(Ew)
+        #Take real part of the IFFT
         RealTime = np.real(Guess_Time)
+        #Go back to frequency space via fourier transform
         Guess_Freq = np.fft.fft(RealTime)
+        #Take new phase in frequency space and input at beginning of alg
         phase_freq = np.angle(Guess_Freq)
-    FinalFreq = Guess_Time
+    FinalFreq = Guess_Freq
     return FinalFreq
 
-def GSTime(iterations,TimeE):
-    TimeAmp = np.abs(TimeE)
-    phase_time = 2*np.pi*np.random.random(len(TimeAmp))
-    for i in range(iterations):
-        Et = np.exp(1j*phase_time)*TimeAmp
-        Guess_Freq = np.fft.fft(Et)
-        RealFreq = np.real(Guess_Freq)
-        Guess_Time = np.fft.ifft(RealFreq)
-        phase_time = np.angle(Guess_Time)
-    FinalTime = np.fft.rifft(RealFreq)
-    return FinalTime
 
-freqs = np.array(pd.read_csv("FreqElectric.csv").iloc[:,1])
-
-Guess_Freq1 = GSFreq(1,FreqAmp)
-Guess_Freq2 = GSFreq(2,FreqAmp)
-Guess_Freq3 = GSFreq(3,FreqAmp)
-#Guess_Freq4 = GSFreq(4,freqs)
-plt.plot(FreqAmp[90:140])
-plt.figure()
-plt.plot(Guess_Freq1)
-plt.plot(Guess_Freq2)
-plt.plot(Guess_Freq3)
-#plt.plot(Guess_Freq4)
 
